f7a593c4a0126eb3d40055f4b92c9b4564e10420
sneha-nacresys <sneha@nacresys.com>
2024-12-10 16:47:01 +0530

    added collectstatic

--- routeoptimization/routeoptimization/settings.py
+++ routeoptimization/routeoptimization/settings.py
@@ -161,3 +161,15 @@ CORS_ALLOW_HEADERS = [
 # ]
 
 
+REACT_APP_DIR = 'static/build/'
+
+STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/build/static')
+    # os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
